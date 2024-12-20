4d62313f6014a5631390f1ba9060cc46abbc70e7
P.D.S.MADHURANGA <2022cs113@stu.ucsc.cmb.ac.lk>
2024-12-20 13:41:45 +0530

    complete order entity

--- TasteTrek-online-food-ordering/src/main/java/com/model/Order.java
+++ TasteTrek-online-food-ordering/src/main/java/com/model/Order.java
@@ -1,14 +1,56 @@
 package com.model;
 
+
+import java.util.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @ManyToOne
+    private User customer;
+
+    @JsonIgnore
+    @ManyToOne
+    private Restaurant restaurant;
+
+    private Long totalAmount;
+
+    private String orderStatus;
+
+    private Date createdAt;
+
+    @JsonIgnore
+    @ManyToOne
+    private Address deliveryAddress;
+    
+    @OneToMany
+    private List<OrderItem> item;
+
+    // private Payment payment;
+
+    private int total;
+
+    private int totalPrice;
+
 }
+
