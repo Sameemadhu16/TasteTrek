b4b1fd4d52df330b655af3e1027508d9d523717c
P.D.S.MADHURANGA <2022cs113@stu.ucsc.cmb.ac.lk>
2024-12-20 14:03:37 +0530

    complete food entity

--- TasteTrek-online-food-ordering/src/main/java/com/model/Food.java
+++ TasteTrek-online-food-ordering/src/main/java/com/model/Food.java
@@ -1,9 +1,17 @@
 package com.model;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,4 +26,30 @@ public class Food {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    private String name;
+
+    private String description;
+
+    private Long price;
+
+    @ManyToOne
+    private Category foodCategory;
+
+    @Column(length = 1000)
+    @ElementCollection
+    private List<String> images;
+
+    private boolean available;
+
+    @ManyToOne
+    private Restaurant restaurant;
+
+    private boolean isVegetarian;
+
+    private boolean isSeasonal;
+
+    @ManyToMany
+    private List<IngredientsItem> ingredients = new ArrayList<>();
+
+    private Date creationDate;
 }
