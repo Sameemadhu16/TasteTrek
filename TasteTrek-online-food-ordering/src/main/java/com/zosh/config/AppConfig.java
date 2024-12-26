65de57dc5f4c7e10e8242dda254b6c4adce622a2
P.D.S.MADHURANGA <2022cs113@stu.ucsc.cmb.ac.lk>
2024-12-26 10:45:51 +0530

    prevent create auto generated password

--- TasteTrek-online-food-ordering/src/main/java/com/zosh/config/AppConfig.java
+++ TasteTrek-online-food-ordering/src/main/java/com/zosh/config/AppConfig.java
@@ -30,7 +30,7 @@ public class AppConfig {
                         .requestMatchers("/api/admin/**").hasAnyRole("RESTAURANT_OWNER", "ADMIN")
                         .requestMatchers("/api/**").authenticated()
                         .anyRequest().permitAll())
-                .addFilterBefore((Filter) new JwtTokenValidator(), BasicAuthenticationFilter.class)
+                .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(corsConfigrationSource()));
 
