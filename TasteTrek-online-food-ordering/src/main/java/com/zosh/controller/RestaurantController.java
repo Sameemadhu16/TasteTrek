6dda38e1b33104bb3ef0ffc49950d234f6626e97
P.D.S.MADHURANGA <2022cs113@stu.ucsc.cmb.ac.lk>
2025-01-02 11:27:09 +0530

    restaurant search method is ready

---
+++ TasteTrek-online-food-ordering/src/main/java/com/zosh/controller/RestaurantController.java
@@ -0,0 +1,44 @@
+package com.zosh.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zosh.model.Restaurant;
+import com.zosh.model.User;
+import com.zosh.request.CreateRestaurantRequest;
+import com.zosh.service.RestaurantService;
+import com.zosh.service.UserService;
+
+@RestController
+@RequestMapping("/api/restaurants")
+public class RestaurantController {
+
+    @Autowired
+    private RestaurantService restaurantService;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/search")
+    public ResponseEntity <List<Restaurant>> searchRestaurant (
+            @RequestHeader("Authorization") String jwt,
+            @RequestParam String keyword
+            ) throws Exception
+        {
+        
+        User user = userService.findUserByJwtToken(jwt);
+        List<Restaurant> restaurant = restaurantService.searchRestaurants(keyword);
+        return new ResponseEntity<>(restaurant, HttpStatus.CREATED);
+    }
+
+}
